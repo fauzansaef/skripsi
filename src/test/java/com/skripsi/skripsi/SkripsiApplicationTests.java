09e6607be3da84cbef1ba05bc15b5f1d848a9653
Fauzan Saef <fauzansaef@gmail.com>
2024-07-19 18:23:08 +0700

    add : project skripsi

---
+++ src/test/java/com/skripsi/skripsi/SkripsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skripsi.skripsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkripsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
